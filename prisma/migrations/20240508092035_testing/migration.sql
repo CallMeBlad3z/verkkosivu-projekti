2940539bc4e3ae36c3cfda0b1b49362d5bf31167
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-08 12:57:07 +0300

    Create migration.sql

---
+++ prisma/migrations/20240508092035_testing/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "orderID" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userID" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("orderID")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "productID" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "categoryID" INTEGER NOT NULL,
+    "orderID" INTEGER,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("productID")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "categoryID" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("categoryID")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "userID" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userID")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userID_fkey" FOREIGN KEY ("userID") REFERENCES "User"("userID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryID_fkey" FOREIGN KEY ("categoryID") REFERENCES "Category"("categoryID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_orderID_fkey" FOREIGN KEY ("orderID") REFERENCES "Order"("orderID") ON DELETE SET NULL ON UPDATE CASCADE;
