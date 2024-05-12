78a402d1d43e2c8f111e8080b3e699ddf65e9c96
Blad3z <santtu366@live.com>
2024-05-12 23:13:17 +0300

    Update user.js
    Implemented login function.

--- src/backend/auth/user.js
+++ src/backend/auth/user.js
@@ -41,4 +41,23 @@ usersRouter.post("/register", async (req, res) => {
 	}
 });
 
+usersRouter.post("/login", async (req, res) => {
+	try {
+		const data = req.body;
+		const user = await prisma.user.findUnique({
+			where: { email: data.email },
+		});
+		if (!user) {
+			return res.status(404).json({ error: "User not found." });
+		}
+		const passwordMatch = bcrypt.compareSync(data.password, user.password);
+		if (!passwordMatch) {
+			return res.status(401).json({ error: "Invalid password." });
+		}
+		res.status(200).json({ user });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
 module.exports = usersRouter;
