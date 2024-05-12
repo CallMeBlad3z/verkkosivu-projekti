b9bbd9bf42661143f2d64dfd3c079ea0ede5bc37
Blad3z <santtu366@live.com>
2024-05-13 01:10:54 +0300

    Update Header.jsx
    Improved code readability, removed an unused import and imported everything from Navbar.jsx, into here.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-       <h1>WebCraft Squad Store</h1>
-    </div>
+    <nav className='navbar'>
+      
+      <div className="pg-title">
+        <li className="nav-btn">
+          <Link to="/">WebCraftSquad Store</Link>
+        </li>
+      </div>
+
+      <div className="navbar-list">
+        <li className="nav-btn">
+          <Link to="/admin">Admin</Link>
+        </li>
+        <li className="nav-btn">
+          <Link to="/user">User</Link>
+        </li>
+        <li className="nav-btn">
+          <Link to="/cart">Cart</Link>
+        </li>
+      </div>
+
+      <div className="li-signin">
+        <li className="nav-btn">
+          <Link to="/login">Login</Link>
+        </li>
+        <li className="nav-btn">
+          <Link to="/register">Register</Link>
+        </li>
+      </div>
+
+    </nav>
   )
 }
 
