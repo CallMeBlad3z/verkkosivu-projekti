7aa79a987083d41a7aeef35ae2025fd782e9d941
Jilves123 <jukka.ilveskorpi@gmail.com>
2024-05-15 10:48:39 +0300

    card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SlickSlider from '../components/SlickSlider'
+import ProductCard from '../components/ProductCard'
 
 const Home = () => {
   return (
@@ -7,6 +8,19 @@ const Home = () => {
         <p>Etusivu pälä pälä höpö höpö</p>
         <br /> 
         <SlickSlider />
+        <br />
+        <br />
+        <ProductCard> </ProductCard>
+        {/*<div
+              className="row"
+              style={{ height: "100vh", overflow: "scroll" }}
+            >
+              {products?.map((p) => (
+                <div className="col-md-4" key={p._id}>
+                  <ProductCard p={p} />
+                </div>
+              ))}
+            </div>*/}
     </div>
   )
 }
