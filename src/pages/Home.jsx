0c70f91ba5386dabe1fe22f7e388d752517451e7
Heikki Gauffin <heikki.gauffin@tuni.fi>
2024-05-16 14:51:49 +0300

    Add front page styling and product category card base component
    Added front page styling for the slick slider images, replaced the original images.. Added product category component and few of them as placeholders to front page without styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,46 @@
 import SlickSlider from '../components/SlickSlider'
 import ProductCard from '../components/ProductCard'
+import ProductCategoryCard from '../components/ProductCategoryCard'
 
 const Home = () => {
   return (
     <div>
-        <p>Etusivu pälä pälä höpö höpö</p>
         <br /> 
         <SlickSlider />
         <br />
         <br />
         <div className="product_row">
+          {/* kehityksenaikainen koodi, kunnes tietokanta kytketty yms,. */}
 
-        {/* kehityksenaikainen koodi, kunnes tietokanta kytketty yms,. */}
+          <div>
+            {Array(2).fill().map((_, i) => (
+              <div className="product_row" key={i}>
+                {Array(3).fill().map((_, j) => (
+                  <ProductCard key={j} />
+                ))}
+              </div>
+            ))}
+          </div>
 
-        <div>
-          {Array(2).fill().map((_, i) => (
-            <div className="product_row" key={i}>
-              {Array(3).fill().map((_, j) => (
-                <ProductCard key={j} />
-              ))}
-            </div>
-          ))}
+            {/* kun tietokanta kytkettynä ym, voi tuotteet mäppää esim suurinpiirtein näin: 
+                <div
+                  className="product_row">
+                  {products?.map((p) => (
+                    <div className="col-md-3" key={p._id}>
+                      <ProductCard p={p} />
+                    </div>
+                  ))}
+                </div>*/}
         </div>
 
-          {/* kun tietokanta kytkettynä ym, voi tuotteet mäppää esim suurinpiirtein näin: 
-              <div
-                className="product_row">
-                {products?.map((p) => (
-                  <div className="col-md-3" key={p._id}>
-                    <ProductCard p={p} />
-                  </div>
-                ))}
-              </div>*/}
-              </div>
-      </div>
+        <div>
+          <ProductCategoryCard />
+          <ProductCategoryCard />
+          <ProductCategoryCard />
+          <ProductCategoryCard />
+        </div>
+
+    </div>
   )
 }
 
