59dc22e68ed9e4a854da6fa7cc3311358610ea54
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-21 10:54:56 +0300

    Create Product.jsx

---
+++ src/pages/Product.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export function ProductPage() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/api/products")
+            .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setProducts(data);
+        });
+}, []);
+
+    return (
+        <section>
+
+            {products.map((product) => (
+                <div key={product.id}>
+                    <h2>{product.title}</h2>
+                    <p>{product.description}</p>
+                    <p>{product.price}</p>
+                    <p>{product.stock}</p>
+                    <p>{product.manufacturer}</p>
+                    <img src={product.image} alt="product" />
+                </div>
+            ))}
+        </section>
+    );
+}
