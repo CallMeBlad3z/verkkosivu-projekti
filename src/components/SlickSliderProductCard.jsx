0281a7434b3d39e4b06a82ca766d1193d7a153f9
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-26 23:18:29 +0300

    Update SlickSliderProductCard.jsx
    Changed to the correct export method and tried fixing the slider, but no avail...

--- src/components/SlickSliderProductCard.jsx
+++ src/components/SlickSliderProductCard.jsx
@@ -3,10 +3,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProductCard from "./ProductCard.jsx";
 
-const SlickSliderProductCard = ({products}) => {
+export default function SlickSliderProductCard({products}) {
   const settings = {
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
     draggable: true,
@@ -15,22 +15,13 @@ const SlickSliderProductCard = ({products}) => {
     arrows:false
   };
 
-  //const numberOfProducts = 10;
-
   return (
     <div>
       <Slider {...settings}>
-        <div>
-					{products.map((p) => (
-						<ProductCard key={p.productID} product={p} />
-					))}
-				</div>
-{/*         {Array.from({ length: numberOfProducts }).map((_, index) => (
-        <ProductCard key={index} />
-        ))} */}
+        {products.map((p) => (
+          <ProductCard key={p.productID} product={p} />
+        ))}
       </Slider>
     </div>
   );
 };
-
-export default SlickSliderProductCard;
