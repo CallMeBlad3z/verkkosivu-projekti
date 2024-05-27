77442d33f4afcfe29c2a9dfae08588df7927ccfb
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-27 09:12:40 +0300

    Update SingleProductView.jsx

--- src/pages/SingleProductView.jsx
+++ src/pages/SingleProductView.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { CartContext } from '../components/CartContext';
 {/* tähän kunkin tuoteryhmän testikuva + lisää sen categoriesdata lootaan tuotteen kohdalle */}
 
 export default function SingleProductView() {
   const [products, setProducts] = useState([]);
+  const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
       fetch(`http://localhost:3000/api/products`)
@@ -14,14 +16,21 @@ export default function SingleProductView() {
   }, []);
   
   return (
-    <div>
+    <div className="product-container">
       {Array.isArray(products) && products.map((product, index) => (
-        <div key={index}>
-            <h1 key={product.title}>{product.title}</h1>
-            <img href={product.image} alt={product.title} />
-            <p>{product.description}</p>
+        <div key={index} className="product-card">
+          <div className="product-image-container">
+            <img className="product-image" src={product.image} alt={product.title} />
+          </div>
+          <div className="product-info">
+            <h1 className="product-title">{product.title}</h1>
+            <p className="product-price">{product.price}€</p>
+            <p className="product-description">{product.description}</p>
+            <p className="product-manufacturer">Manufacturer: {product.manufacturer}</p>
+            <button className="add-to-cart-button" onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
         </div>
-    ))}
+      ))}
     </div>
-);
+  );
 }
