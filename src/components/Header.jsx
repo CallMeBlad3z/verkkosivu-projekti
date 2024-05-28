f1dfb5647b5c851682629044dc364baf39f426c7
Terviz <65880299+Terviz@users.noreply.github.com>
2024-05-28 09:55:29 +0300

    Further stylings to header component and replaced cart text with shopping cart icon
    Further stylings to header component and replaced cart text with shopping cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,47 @@
 import { Link } from "react-router-dom";
+import { FiShoppingCart } from 'react-icons/fi';
 
 const Header = ({ user, handleLogout }) => {
 	return (
 		<nav className="navbar">
-			<div className="pg-title">
+			<div className="pg-title didact-gothic-regular">
 				<li className="nav-btn">
-					<Link to="/">WebCraftSquad Store</Link>
+					<Link to="/">Pohjolan Bazaari</Link>
 				</li>
 			</div>
-
-			<div className="navbar-list">
-				{user !== null && user.isAdmin && (
+			<div className="navbar-right-content-container didact-gothic-regular">
+				<div className="navbar-list">
+					{user !== null && user.isAdmin && (
+						<li className="nav-btn">
+							<Link to="/admin">Admin</Link>
+						</li>
+					)}
 					<li className="nav-btn">
-						<Link to="/admin">Admin</Link>
+						<Link to="/cart"><FiShoppingCart/></Link>
 					</li>
+				</div>
+				{user === null ? (
+					<div className="li-signin">
+						<li className="nav-btn">
+							<Link to="/login">Login</Link>
+						</li>
+						<li className="nav-btn">
+							<Link to="/register">Register</Link>
+						</li>
+					</div>
+				) : (
+					<div>
+						<li className="nav-btn">
+							<Link to="/user">{user.firstname}</Link>
+						</li>
+						<li className="nav-btn">
+							<Link onClick={handleLogout} to="/">
+								Log out
+							</Link>
+						</li>
+					</div>
 				)}
-				<li className="nav-btn">
-					<Link to="/cart">Cart</Link>
-				</li>
 			</div>
-			{user === null ? (
-				<div className="li-signin">
-					<li className="nav-btn">
-						<Link to="/login">Login</Link>
-					</li>
-					<li className="nav-btn">
-						<Link to="/register">Register</Link>
-					</li>
-				</div>
-			) : (
-				<div>
-					<li className="nav-btn">
-						<Link to="/user">{user.firstname}</Link>
-					</li>
-					<li className="nav-btn">
-						<Link onClick={handleLogout} to="/">
-							Log out
-						</Link>
-					</li>
-				</div>
-			)}
 		</nav>
 	);
 };
